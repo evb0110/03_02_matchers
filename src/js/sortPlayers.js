06c0fe39738e8d7678128f7510d388ea0a421bf9
evb0110 <7149553@gmail.com>
2019-01-25 16:23:12 +0300

    solved

--- src/js/sortPlayers.js
+++ src/js/sortPlayers.js
@@ -1,5 +1,26 @@
 const healthComparator = (a, b) => a.health - b.health;
 
-const sortPlayers = players => players.sort(healthComparator);
+const validateInput = (input) => {
+  if (!Array.isArray(input)) {
+    throw new Error('should receive an array as input');
+  }
+};
+
+const validatePlayer = (player) => {
+  if (!(typeof player === 'object')) {
+    throw new Error('invalid input: player must be an object');
+  }
+  const { name, health } = player;
+  if (!(typeof name === 'string') || !(typeof health === 'number')) {
+    throw new Error('wrong or missing properties of player');
+  }
+  return true;
+};
+
+const sortPlayers = (players) => {
+  validateInput(players);
+  players.forEach(player => validatePlayer(player));
+  return players.sort(healthComparator);
+};
 
 export default sortPlayers;
--- src/js/sortPlayers.test.js
+++ src/js/sortPlayers.test.js
@@ -1,5 +1,16 @@
 import sortPlayers from './sortPlayers';
 
+test('input not an array throws', () => {
+  const heroes = 1;
+  expect(() => sortPlayers(heroes)).toThrow();
+});
+
+test('input with player not an object throws', () => {
+  const heroes = [1];
+  expect(() => sortPlayers(heroes)).toThrow();
+});
+
+
 test('sort simple input', () => {
   const heroes = [{
     name: 'мечник',
@@ -29,4 +40,18 @@ test('sort simple input', () => {
   ];
   const sorted = sortPlayers(heroes);
   expect(sorted).toEqual(expected);
+});
+
+test('wrong input throws', () => {
+  const heroes = [
+    {
+      name: 'эльф',
+      health: 'маг',
+    },
+    {
+      name: 'маг',
+      health: 20,
+    },
+  ];
+  expect(() => sortPlayers(heroes)).toThrow();
 });
